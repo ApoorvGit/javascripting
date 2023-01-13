7e3ca4084e1f10ada0fc7173d7de84b3bb97e24c
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-13 15:54:32 +0530

    fix: Updated the declaration type of functions

--- ques.js
+++ ques.js
@@ -1,16 +1,17 @@
-function isPrime(x){
-    for(let i=2;i<x;i=i+1){
+const isPrime=(x)=>{
+    if(x%2==0) return false
+    for(let i=2;i<x;i=i+2){
         if(x%i==0) return false
     }
     return true 
 }
 
-function isArm(xi){
+const isArm=(xi)=>{
     let x=xi
     let total=0
     while(x>0){
         let i=x%10
-        console.log(i)
+        // console.log(i)
         x=Math.floor(x/10)
         total=total+i*i*i
     }
