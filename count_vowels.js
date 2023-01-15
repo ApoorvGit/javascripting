428da41262a798afc1211de5c8c04e34fa4bf76c
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-16 00:54:16 +0530

    feat: add functionality to count number of vowels

---
+++ count_vowels.js
@@ -0,0 +1,9 @@
+function count_vow(str){
+    return count = str.match(/[aeiou]/gi).length
+}
+const arrow_count_vow=(str)=>{
+    return count = str.match(/[aeiou]/gi).length
+}
+
+console.log(count_vow("apoorv"))
+console.log(arrow_count_vow("codeacademy"))
