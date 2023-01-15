9b8d59d51cdaa904c13e8eafe0e63d5f783abe15
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-16 00:15:40 +0530

    feat: add functionality to check if a string ends with a specific word

---
+++ ends_with.js
@@ -0,0 +1,10 @@
+function ends(str){
+    if(str.endsWith("Script")) return true
+    return false
+}
+const arrow_ends=(str)=>{
+    if(str.endsWith("Script")) return true
+    return false
+}
+console.log(ends("JavaScript"))
+console.log(ends("apoorv"))
