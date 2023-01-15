e97fa263f4e34f2e203074fa73baa34b4346c5e5
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-16 00:53:38 +0530

    feat: add functionality to remove first charcter and concatenate the string

---
+++ concat.js
@@ -0,0 +1,8 @@
+function delete_and_concat(str1, str2){
+    return str1.slice(1)+str2.slice(1)
+}
+const arrow_delete_and_concat=(str1, str2)=>{
+    return str1.slice(1)+str2.slice(1)
+}
+console.log(delete_and_concat("apoorv", "mishra"))
+console.log(arrow_delete_and_concat("code", "academy"))
