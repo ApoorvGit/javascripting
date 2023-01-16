d9a082a1c3a7844084786329e69a6d7e6bacdbf9
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-16 13:38:17 +0530

    fix: add functionality to import the functions

--- concat.js
+++ concat.js
@@ -6,3 +6,8 @@ const arrow_delete_and_concat=(str1, str2)=>{
 }
 console.log(delete_and_concat("apoorv", "mishra"))
 console.log(arrow_delete_and_concat("code", "academy"))
+
+module.exports={
+    delete_and_concat,
+    arrow_delete_and_concat
+}
