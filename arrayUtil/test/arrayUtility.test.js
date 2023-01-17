926609eefab218bcfad5818562482b3a8985b622
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-17 09:58:00 +0530

    chores: add testcase to test functionality to tripple numbers and filter even numbers

--- arrayUtil/test/arrayUtility.test.js
+++ arrayUtil/test/arrayUtility.test.js
@@ -1,4 +1,4 @@
-const {doubleNumber, filterEvenNumber}=require('../arrayUtility');
+const {doubleNumber, filterEvenNumber, tripleNumberAndzfilterEven}=require('../arrayUtility');
 
 
 describe('Array Utilities', ()=>{
@@ -6,7 +6,7 @@ describe('Array Utilities', ()=>{
         it('should invoke an error when input is not an array',()=>{
             expect(()=>doubleNumber('abc')).toThrow('Type Error');
         });
-        it('should invoke an error when input is not an array',()=>{
+        it('should double each number when input is an array',()=>{
             const result=doubleNumber([1,2,3]);
             expect(result).toEqual([2, 4, 6]);
         });
@@ -16,11 +16,20 @@ describe('Array Utilities', ()=>{
         it('should invoke an error when input is not an array',()=>{
             expect(()=>filterEvenNumber('abc')).toThrow('Type Mismatch');
         });
-        it('should invoke an error when input is not an array',()=>{
+        it('should filter even numbers when input is an array',()=>{
             const result=filterEvenNumber([1,2,3,4,5,6]);
             expect(result).toEqual([2, 4, 6]);
         });
     });
+    describe('Triple each number and Filter Even Number', ()=>{
+        it('should invoke an error when input is not an array',()=>{
+            expect(()=>tripleNumberAndzfilterEven('abc')).toThrow('Type Error');
+        });
+        it('should triple each number and then filter even numbers when input is an array',()=>{
+            const result=tripleNumberAndzfilterEven([1,2,3,4,5,6]);
+            expect(result).toEqual([6,12,18]);
+        });
+    });
     
-    
+
 });
