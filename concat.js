e53c57b26c3e68d68cdb5f4b221acd156f4076da
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-18 09:08:00 +0530

    fix: add condition to check that inputs are  always strings

--- concat.js
+++ concat.js
@@ -1,13 +1,13 @@
 function delete_and_concat(str1, str2){
-    return str1.slice(1)+str2.slice(1)
+    return str1.slice(1)+str2.slice(1);
 }
 const arrow_delete_and_concat=(str1, str2)=>{
-    return str1.slice(1)+str2.slice(1)
-}
-console.log(delete_and_concat("apoorv", "mishra"))
-console.log(arrow_delete_and_concat("code", "academy"))
+    return str1.slice(1)+str2.slice(1);
+};
+console.log(delete_and_concat('apoorv', 'mishra'));
+console.log(arrow_delete_and_concat('code', 'academy'));
 
 module.exports={
     delete_and_concat,
     arrow_delete_and_concat
-}
+};
