f06511764ba1b392d06d1afada82d30ad7d188ae
apoorvGit <apoorvmishra1000@gmail.com>
2023-01-18 09:05:36 +0530

    devops: add new test cases to test functionality of string concatenation

--- test/concat.test.js
+++ test/concat.test.js
@@ -1,6 +1,6 @@
-const {delete_and_concat, arrow_delete_and_concat}=require('../concat.js')
+const {delete_and_concat, arrow_delete_and_concat}=require('../concat.js');
 
 test('testing delete the first characters and then concat functioanlity', ()=>{
-    expect(delete_and_concat("code", "academy")).toBe("odecademy")
-    expect(arrow_delete_and_concat("Apoorv", "Mishra")).toBe("poorvishra")
-})
+    expect(delete_and_concat('code', 'academy')).toBe('odecademy');
+    expect(arrow_delete_and_concat('Apoorv', 'Mishra')).toBe('poorvishra');
+});
